2c2d7b8a6724008862315078e62ecac9ef47355b
Xin Ye <yexinwhu@gmail.com>
2021-02-03 21:51:10 -0600

    day 25 part 1

---
+++ 2020/day_25.py
@@ -0,0 +1,40 @@
+#--- Day 25: Combo Breaker ---
+
+card_public_key = 9717666
+door_public_key = 20089533
+
+#--- part 1 ---
+
+subject_number = 7
+divisor = 20201227
+
+card_loop_size = 0
+door_loop_size = 0
+
+found1 = False
+found2 = False
+
+key = 1
+
+while not found1 or not found2:
+
+	if not found1:
+		card_loop_size += 1
+	if not found2:
+		door_loop_size += 1
+
+	key *= subject_number
+	key %= divisor
+
+	if key == card_public_key:
+		found1 = True
+	if key == door_public_key:
+		found2 = True
+
+encryption_key = 1
+
+for i in range(card_loop_size):
+	encryption_key *= door_public_key
+	encryption_key %= divisor
+
+print(encryption_key)
