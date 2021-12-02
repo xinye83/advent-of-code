3b5841558a1dea959bae0ed9d4b9f31fcad6161f
Xin Ye <yexinwhu@gmail.com>
2021-12-01 22:20:31 -0600

    minor change

--- 2021/day01.py
+++ 2021/day01.py
@@ -6,15 +6,16 @@ depth = data[0]
 count = 0
 
 for i, d in enumerate(depth):
-    if i > 0 and d > depth[i-1]:
+    if i > 0 and d > depth[i - 1]:
         count += 1
 
 print(f"Part 1 - {count}")
 
 count = 0
+distance = 2
 
 for i, d in enumerate(depth):
-    if i > 2 and d > depth[i-3]:
+    if i > distance and d > depth[i - distance - 1]:
         count += 1
 
 print(f"Part 2 - {count}")
